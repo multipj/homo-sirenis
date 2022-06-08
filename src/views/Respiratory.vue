0dcd494735eac9917f45151468ca24e7be8e0df5
dkaerit <kaerit@hotmail.com>
2022-06-08 01:38:30 +0100

    breath

--- src/views/Respiratory.vue
+++ src/views/Respiratory.vue
@@ -4,7 +4,7 @@
     <Header/>
     <div class="contain">
       <h1>Aparato respiratorio</h1>
-      <Markdown :source="home" class="setp"/>
+      <Markdown :source="md" class="setp"/>
       <router-link to="/" tag="button" class="button-back">Atrás</router-link>
     </div>
     
@@ -15,14 +15,14 @@
 import Bar from "@/components/Bar.vue";
 import Header from "@/components/Header.vue";
 import Markdown from 'vue3-markdown-it';
-import home_md from '@/markdown/example.md';
+import md from '@/markdown/respiratory.md';
 import $ from 'jquery';
 
 export default {
   name: 'Breath',
   data: function() {
     return {
-      home: home_md,
+      md: md,
     }
   },
   methods: {
